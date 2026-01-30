69dfbcc82184acd3d061451152e976a3190dd8de
Pavan Kakde <pavankakde2006@gmail.com>
2026-01-30 22:52:00 +0530

    new

--- chapter-6/user/new.js
+++ chapter-6/user/new.js
@@ -4,12 +4,12 @@ console.log('1. Start of script');
 
 // Synchronous (blocking) operation
 console.log('2. Reading file synchronously');
-const dataSync = fs.readFileSync('user-details.txt', 'utf8');
+const dataSync = fs.readFileSync('user.txt', 'utf8');
 console.log('3. Synchronous read complete');
 
 // Asynchronous (non-blocking) operation
 console.log('4. Reading file asynchronously');
-fs.readFile('user-details.txt', 'utf8', (err, dataAsync) => {
+fs.readFile('user.txt', 'utf8', (err, dataAsync) => {
   if (err) throw err;
   console.log('6. Asynchronous read complete');
 });
